caece7e922ca7ce1accafd1ce3511c3b7724685b
Roman S <ua.romansyslikov@gmail.com>
2020-09-12 02:52:31 +0300

    0.2.0

--- modules/main/src/index.ts
+++ modules/main/src/index.ts
@@ -1,6 +1,9 @@
-import { createScope } from "@replikit/core";
+import { AttachmentType, createScope, TextTokenKind } from "@replikit/core";
 import { router } from "@replikit/router";
 import { fetch } from "cross-fetch";
+import "@replikit/messages";
+import { MessageBuilder } from "@replikit/messages";
+import { type } from "os";
 
 export const logger = createScope("assistent");
 
@@ -12,19 +15,48 @@ router.of("message:received").use(async context => {
   if (text != null) {
     const result = await fetch(link, {
       method: 'POST',
-      body: JSON.stringify({ Text: text }),
+      body: JSON.stringify({ Text: text, IsNullWithoutAssistantKey: true }),
       headers: {
         "Content-Type": "application/json"
       }
     });
+
+    if (result.status == 204) return;
     
     if (result.status != 200) {
       throw new Error("Bad response from server " + result.status);
     }
     
     const data = await result.json();
-  
     console.log(data);
-    context.reply(data["Text"]);
+    const attachment = data.Attachment;
+
+    var builder = new MessageBuilder()
+      .addText(data.Text);
+
+    //builder.addAttachmentByUrl(AttachmentType.Video, "https://romanflx.000webhostapp.com/video_6.mp4");
+
+    //builder.addAttachmentByUrl(AttachmentType.Photo, "https://i.ytimg.com/vi/et4xUWhz0X0/maxresdefault.jpg");
+
+    if (attachment != null)
+    {
+      switch (attachment.Name) {
+        case "LinkAttachment":
+          builder.addText("\n\n")
+          builder.addToken({
+            kind: TextTokenKind.Link,
+            text: attachment.Text,
+            url: attachment.Link,
+            props: []
+          });
+          break;
+        case "ImagesAttachment":
+          attachment.Images.forEach((e:any) => {
+            builder.addAttachmentByUrl(AttachmentType.Photo, e.ImageLink);
+          });
+          break;
+      }
+    }
+    context.reply(builder);
   }
 });
--- replikit.config.ts
+++ replikit.config.ts
@@ -1,6 +1,8 @@
 import { Configuration } from "@replikit/core/typings";
 
 import "@replikit/vk";
+import "@replikit/telegram";
+import "@replikit/discord";
 import "@assistant-ts-client/main"
 
 const config: Configuration = {
@@ -8,6 +10,12 @@ const config: Configuration = {
     token: process.env.VK_TOKEN!,
     pollingGroup: +process.env.VK_GROUP!
   },
+  telegram: {
+    token: process.env.TG_TOKEN!
+  },
+  discord: {
+    token: process.env.DC_TOKEN!
+  }
 }
 
 export default config
